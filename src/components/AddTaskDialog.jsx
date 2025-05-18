2b69551c09ed6c5ce7263a050465ef85660f4c1e
Gabriel <gabrielqueiroz2510@gmail.com>
2025-05-18 16:14:04 -0300

    feat: ensure form is cleared when closing AddTaskDialog

--- src/components/AddTaskDialog.jsx
+++ src/components/AddTaskDialog.jsx
@@ -60,6 +60,15 @@ const AddTaskDialog = ({
     })
   }
 
+  const handleCancelClick = () => {
+    reset({
+      title: '',
+      time: 'morning',
+      description: '',
+    })
+    handleClose()
+  }
+
   return (
     <CSSTransition
       nodeRef={nodeRef}
@@ -134,7 +143,7 @@ const AddTaskDialog = ({
                     size="large"
                     className="w-full"
                     color="secondary"
-                    onClick={handleClose}
+                    onClick={handleCancelClick}
                     type="button"
                   >
                     Cancelar

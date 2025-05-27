596352e3a7bce42e6e11b60bf8b7261aca63c93c
Gabriel <gabrielqueiroz2510@gmail.com>
2025-05-27 11:37:13 -0300

    feat: add home and tasks page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Sidebar from '../components/Sidebar'
+
+const HomePage = () => {
+  return (
+    <div className="flex">
+      <Sidebar></Sidebar>
+    </div>
+  )
+}
+
+export default HomePage

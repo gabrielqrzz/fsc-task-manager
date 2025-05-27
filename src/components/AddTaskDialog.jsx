1c3fc051e414e0692858981247e50f05725e031c
Gabriel <gabrielqueiroz2510@gmail.com>
2025-05-27 10:17:13 -0300

    feat: add hooks to handle task addition, deletion and fetching

--- src/components/AddTaskDialog.jsx
+++ src/components/AddTaskDialog.jsx
@@ -1,6 +1,5 @@
 import './AddTaskDialog.css'
 
-import { useMutation, useQueryClient } from '@tanstack/react-query'
 import PropTypes from 'prop-types'
 import { useRef } from 'react'
 import { createPortal } from 'react-dom'
@@ -10,26 +9,14 @@ import { toast } from 'sonner'
 import { v4 } from 'uuid'
 
 import { LoaderIcon } from '../assets/icons'
+import { useAddTask } from '../hooks/data/use-add-task'
 import Button from './Button'
 import Input from './Input'
 import InputLabel from './InputLabel'
 import TimeSelect from './TimeSelect'
 
 const AddTaskDialog = ({ isOpen, handleClose }) => {
-  const queryClient = useQueryClient()
-  const { mutate } = useMutation({
-    mutationKey: 'addTask',
-    mutationFn: async (task) => {
-      const response = await fetch('http://localhost:3000/tasks', {
-        method: 'POST',
-        body: JSON.stringify(task),
-      })
-      if (!response.ok) {
-        throw new Error()
-      }
-      return response.json()
-    },
-  })
+  const { mutate: addTask } = useAddTask()
   const {
     register,
     formState: { errors, isSubmitting },
@@ -54,11 +41,8 @@ const AddTaskDialog = ({ isOpen, handleClose }) => {
       description: data.description.trim(),
       status: 'not_started',
     }
-    mutate(task, {
+    addTask(task, {
       onSuccess: () => {
-        queryClient.setQueryData('tasks', (currentTasks) => {
-          return [...currentTasks, task]
-        })
         toast.success('Tarefa adicionada com sucesso.')
         handleClose()
         reset({
--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,4 +1,3 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 import { toast } from 'sonner'
@@ -9,25 +8,14 @@ import {
   LoaderIcon,
   TrashIcon,
 } from '../assets/icons/index.js'
+import { useDeleteTask } from '../hooks/data/use-delete-task.js'
 import Button from './Button'
 
 const TaskItem = ({ task, handleCheckboxClick }) => {
-  const queryClient = useQueryClient()
-  const { mutate, isPending } = useMutation({
-    mutationKey: ['deleteTask', task.id],
-    mutationFn: async () => {
-      const response = await fetch(`http://localhost:3000/tasks/${task.id}`, {
-        method: 'DELETE',
-      })
-      return response.json()
-    },
-  })
+  const { mutate: deleteTask, isPending } = useDeleteTask(task.id)
   const handleDeleteClick = async () => {
-    mutate(undefined, {
+    deleteTask(undefined, {
       onSuccess: () => {
-        queryClient.setQueryData('tasks', (currentTasks) => {
-          return currentTasks.filter((currentTask) => currentTask.id != task.id)
-        })
         toast.success('Tarefa deletada com sucesso!')
       },
       onError: () => {
